68e4db6b385fdd5a462ab24e24912f2f371c2bc4
Sitecore Whiz <sitecorewhiz@gmail.com>
2019-07-31 15:21:12 -0400

    Commiting pubsub project to git
    Commiting pubsub project to git

---
+++ MSMQPubSub/Publisher/PublisherEntrypoint.cs
@@ -0,0 +1,57 @@
+﻿using MessagePayLoad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Publisher
+{
+    public class PublisherEntryPoint
+    {
+        public static void Main(string[] args)
+        {
+            MessageQueue messageQueue = null;
+
+            string path = @".\Private$\pubsub";
+
+            Console.WriteLine("Enter a message to transmit.... Press x to exit");
+            string input = Console.ReadLine();
+
+            while (input != "x")
+            {
+                PayLoad payLoad = new PayLoad { MessageBody = input };
+
+                try
+                {
+                    Message msg = new Message();
+                    msg.Label = "Payload from Publisher";
+
+
+                    msg.Body = payLoad;
+
+
+                    if (!MessageQueue.Exists(path))
+                    {
+                        //No Q exists, hence create it
+                        messageQueue = MessageQueue.Create(path);
+                    }
+                    else
+                    {
+                        messageQueue = new MessageQueue(path);
+                    }
+                    messageQueue.Send(payLoad);
+
+                    Console.WriteLine($"Message {input} transmitted to the Queue");
+                    Console.WriteLine("Enter a message to transmit.... Press x to exit");
+                    input = Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{ex.Message}");
+                }
+            }
+        }
+    }
+}
---
+++ MSMQPubSub/SubscriberA/SubscriberAEntryPoint.cs
@@ -0,0 +1,62 @@
+﻿using MessagePayLoad;
+using System;
+using System.Collections.Generic; 
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubscriberA
+{
+   public class SubscriberAEntryPoint
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hit S to subscribe to Publisher at any time... Press X to exit");
+     
+            string input = Console.ReadLine();
+
+            List<PayLoad> allMessages = new List<PayLoad>();
+
+            string path = @".\Private$\pubsub";
+
+            while(input=="s" || input != "x")
+            {
+                if(input=="u")
+                {
+                    Console.WriteLine("Hit S to subscribe and view all messages.... Press X to exit");
+                    input = Console.ReadLine();
+                    if (input == "s")
+                        continue;
+                    else if (input == "u" || input == "x")
+                        break;
+                }
+
+                try
+                {
+                    using (MessageQueue messageQueue = new MessageQueue(path))
+                    {
+                        Message[] messages = messageQueue.GetAllMessages();
+                        if(messages.Count()>0)
+                        {
+                            foreach (Message msg in messages)
+                            {
+                                msg.Formatter = new XmlMessageFormatter(new Type[] { typeof(PayLoad) });
+                                var receivedMessage = (PayLoad)msg.Body;
+                                Console.WriteLine($"Message Received from Publisher {msg.Label} is:{receivedMessage.MessageBody}");
+                            }
+                        }
+                    }
+
+                    Console.WriteLine("Hit U to subscribe or unsubscribe to Publisher at any time.... Press x to exit");
+                    Console.WriteLine("Press any other key to retrieve messages from Publisher");
+                    input = Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
